99e381e16196924bb3085340c047a966a1ea8d22
André Kirst <github@andrekirst.de>
2019-07-22 13:28:38 +0200

    Refactoring

--- Haushaltsbuch.Library/Infrastructure/Implementations/RepositoryException.cs
+++ Haushaltsbuch.Library/Infrastructure/Implementations/RepositoryException.cs
@@ -4,7 +4,7 @@ using System.Runtime.Serialization;
 namespace Haushaltsbuch.Library.Infrastructure.Implementations
 {
     [Serializable]
-    internal class RepositoryException : Exception
+    public class RepositoryException : Exception
     {
         public RepositoryException()
         {
